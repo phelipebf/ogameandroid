00575494c5434d1ae011490c57f4c02ae4cf75f3
overkillerror <overkillerror@4f8b7dd1-b00a-0636-874a-41ac41d9f6d2>
2011-03-29 14:06:46 +0000

    updated German translation added folder for Polish translation fixed some warnings

--- trunk/src/com/overkill/ogame/MessageComposeView.java
+++ trunk/src/com/overkill/ogame/MessageComposeView.java
@@ -8,7 +8,6 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
